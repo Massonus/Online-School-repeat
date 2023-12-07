8a20898857e83eea31910a6da7fc24a52be75a88
makso <maksym.lithuanian@gmail.com>
2023-12-07 14:21:30 +0200

    check

--- src/main/java/org/massonus/Main.java
+++ src/main/java/org/massonus/Main.java
@@ -4,6 +4,7 @@ import org.massonus.utils.CourseController;
 
 public class Main {
     public static void main(String[] args) {
+
         CourseController courseController = new CourseController();
         courseController.firstCreate();
         courseController.mainMenu();
