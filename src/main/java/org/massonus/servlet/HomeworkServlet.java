e480add3eef2b3fbd0990d0baac08531846d182e
makso <maksym.lithuanian@gmail.com>
2024-01-04 19:00:32 +0200

    Task 49 full completed

--- src/main/java/org/massonus/servlet/HomeworkServlet.java
+++ src/main/java/org/massonus/servlet/HomeworkServlet.java
@@ -5,19 +5,26 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.massonus.entity.Homework;
+import org.massonus.repo.HomeworkRepo;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
-@WebServlet("/hello")
-public class Hello extends HttpServlet {
+@WebServlet(value = "/homework")
+public class HomeworkServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
 
-        PrintWriter printWriter = resp.getWriter();
+        final HomeworkRepo homeworkRepo = new HomeworkRepo();
+        final List<Homework> homeworkList = homeworkRepo.getAllHomework();
 
-        printWriter.write("Hello World!");
-        printWriter.close();
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h2>Hello " + homeworkList + "</h2>");
+
+        writer.close();
     }
 }
---
+++ src/main/java/org/massonus/servlet/PersonServlet.java
@@ -0,0 +1,30 @@
+package org.massonus.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.massonus.entity.Person;
+import org.massonus.repo.PersonRepo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(value = "/people")
+public class PersonServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        final PersonRepo personRepo = new PersonRepo();
+        final List<Person> people = personRepo.getAllPeople();
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("<h2>Hello " + people + "</h2>");
+
+        writer.close();
+    }
+}
